ede473112126a2d70d17e90585ac6df6d56d038d
santos177 <santos177@gmail.com>
2018-08-01 01:47:11 -0400

    errors in utils files, fixed

--- src/rpc/protocol.cpp
+++ src/rpc/protocol.cpp
@@ -69,7 +69,7 @@ static const std::string COOKIEAUTH_FILE = ".cookie";
 fs::path GetAuthCookieFile()
 {
     fs::path path(gArgs.GetArg("-rpccookiefile", COOKIEAUTH_FILE));
-    if (!path.is_complete()) path = GetDataDir(true) / path;
+    if (!path.is_complete()) path = GetDataDir() / path;
     return path;
 }
 
